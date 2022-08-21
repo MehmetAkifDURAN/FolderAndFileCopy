c4307195c10ee9ec0d385febc36ec0bd922eb843
MehmetAkifDuran <mehmetakifduran2020@gmail.com>
2022-08-21 23:41:11 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace ObjectOrientedProgramming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceTextBox = new System.Windows.Forms.TextBox();
+            this.targetTextBox = new System.Windows.Forms.TextBox();
+            this.sourceLabel = new System.Windows.Forms.Label();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.targetLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sourceTextBox
+            // 
+            this.sourceTextBox.Location = new System.Drawing.Point(100, 20);
+            this.sourceTextBox.Name = "sourceTextBox";
+            this.sourceTextBox.Size = new System.Drawing.Size(325, 20);
+            this.sourceTextBox.TabIndex = 0;
+            // 
+            // targetTextBox
+            // 
+            this.targetTextBox.Location = new System.Drawing.Point(100, 60);
+            this.targetTextBox.Name = "targetTextBox";
+            this.targetTextBox.Size = new System.Drawing.Size(325, 20);
+            this.targetTextBox.TabIndex = 1;
+            // 
+            // sourceLabel
+            // 
+            this.sourceLabel.AutoSize = true;
+            this.sourceLabel.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sourceLabel.Location = new System.Drawing.Point(20, 20);
+            this.sourceLabel.Name = "sourceLabel";
+            this.sourceLabel.Size = new System.Drawing.Size(59, 21);
+            this.sourceLabel.TabIndex = 2;
+            this.sourceLabel.Text = "Kaynak";
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Location = new System.Drawing.Point(450, 20);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(75, 60);
+            this.copyBtn.TabIndex = 3;
+            this.copyBtn.Text = "Copy";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            this.copyBtn.Click += new System.EventHandler(this.copyBtn_Click);
+            // 
+            // targetLabel
+            // 
+            this.targetLabel.AutoSize = true;
+            this.targetLabel.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.targetLabel.Location = new System.Drawing.Point(20, 60);
+            this.targetLabel.Name = "targetLabel";
+            this.targetLabel.Size = new System.Drawing.Size(55, 21);
+            this.targetLabel.TabIndex = 2;
+            this.targetLabel.Text = "Hedef";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 100);
+            this.Controls.Add(this.copyBtn);
+            this.Controls.Add(this.targetLabel);
+            this.Controls.Add(this.sourceLabel);
+            this.Controls.Add(this.targetTextBox);
+            this.Controls.Add(this.sourceTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Klasör ve Dosya Kopyala";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sourceTextBox;
+        private System.Windows.Forms.TextBox targetTextBox;
+        private System.Windows.Forms.Label sourceLabel;
+        private System.Windows.Forms.Button copyBtn;
+        private System.Windows.Forms.Label targetLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ObjectOrientedProgramming
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void copyBtn_Click(object sender, EventArgs e)
+        {
+            string sourcePath = sourceTextBox.Text;
+            string targetPath = targetTextBox.Text;
+            if (sourceTextBox.Text == "" || targetTextBox.Text == "")
+            {
+                MessageBox.Show("Kaynak dizin ve/veya hedef dizin yolu gereklidir", "Uyarı");
+            }
+            else
+            {
+                CopyAll(sourcePath, targetPath);
+            }
+        }
+
+        private void CopyAll(string source, string target)
+        {
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(source);
+                DirectoryInfo[] subDirectories = directoryInfo.GetDirectories();
+                FileInfo[] files = directoryInfo.GetFiles();
+
+                if (!Directory.Exists(target))
+                {
+                    Directory.CreateDirectory(target);
+                }
+
+                if (subDirectories.Length > 0)
+                {
+                    foreach (var subDirectory in subDirectories)
+                    {
+                        foreach (var file in files)
+                        {
+                            File.Copy(source + "\\" + file, target + "\\" + file, true);
+                        }
+                        Directory.CreateDirectory(target + "\\" + subDirectory.Name);
+
+                        CopyAll(source + "\\" + subDirectory.Name, target + "\\" + subDirectory.Name);
+                    }
+                }
+                else
+                {
+                    foreach (var file in files)
+                    {
+                        File.Copy(source + "\\" + file, target + "\\" + file, true);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException exception)
+            {
+                MessageBox.Show("Kaynak ve/veya hedef klasörün yolu bulunamadı", "Uyarı");
+            }
+            catch (IOException exception)
+            {
+                MessageBox.Show("Dosya kullanılırken kopyalama yapamazsınız", "Uyarı");
+            }
+            catch
+            {
+                MessageBox.Show("Beklenmeyen bir hata oluştu", "Uyarı");
+            }
+        }
+    }
+}
